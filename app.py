bb1b39b080daef51c63fa8cc51d94d83e14f9272
root <root@DESKTOP-12GFGR1.localdomain>
2022-02-23 21:40:29 +0100

    first push

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:quanghuy123456@localhost:5432/flasksql'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class People(db.Model):
+    __tablename__='people'	
+
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, pname, color):
+        self.pname = pname
+        self.color = color
+
+@app.route('/')
+def home():
+    return '<a href="/addperson"><button> Click here </button></a>'
+
+
+@app.route("/addperson")
+def addperson():
+    return render_template("index.html")
+
+
+@app.route("/personadd", methods=['POST'])
+def personadd():
+    pname = request.form["pname"]
+    color = request.form["color"]
+    entry = People(pname, color)
+    db.session.add(entry)
+    db.session.commit()
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
